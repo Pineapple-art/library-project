702fcf9eac626d15e316330427ede6f653a64afc
pineapple <187630489+Pineapple-art@users.noreply.github.com>
2025-07-17 18:46:54 +0200

    refactor: replace logic with classes and encapsulate initial library with IIFE

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-const myLibrary = []
 
 class Book {
-  constructor(title, author, pages, read, myLibrary) {
+  constructor(title, author, pages, read) {
     this.id = crypto.randomUUID()
     this.title = title;
     this.author = author;
@@ -13,25 +12,64 @@ class Book {
     }
   }
 
-  addBookToLibrary(myLibrary) {
-    myLibrary.push(this)
+  toggleRead() {
+    this.isRead = !this.isRead
+  }
+}
+
+class Library {
+  constructor(library = []) {
+    this.library = library;
   }
 
-  addBookToPage() {
+  addBook(book) {
+    this.library.push(book)
+  }
+  
+  deleteBook(bookId) { // delete from array
+    const index = this.library.findIndex((item) => bookId === item.id)
+    if (index >= 0) {
+      this.library.splice(index, 1)
+    }
+  }
+}
+
+class Display {
+
+  static addBook(newBook) {
     const bookCard = document.createElement('li')
     const book = document.createElement('p')
 
-    bookCard.setAttribute("data-book-id", this.id)
-    book.textContent = this.info()
+    bookCard.setAttribute("data-book-id", newBook.id)
+    book.textContent = newBook.info()
     bookCard.classList.add("content")
 
     bookList.appendChild(bookCard);
     bookCard.appendChild(book)
-    this.addToggleBtn(bookCard)
-    this.addDeleteBtn(bookCard)
+    this.#addDeleteBtn(bookCard)
+    this.#addToggleBtn(bookCard, newBook.isRead)
   }
 
-  addDeleteBtn(bookCard) {
+  static #addToggleBtn(bookCard, isRead) {
+    const changeBtn = document.createElement('button')
+
+    isRead ? changeBtn.textContent = "Read"
+      : changeBtn.textContent = "Not read"
+
+    changeBtn.classList.add("change")
+    bookCard.appendChild(changeBtn);
+
+    changeBtn.addEventListener("click", () => {
+      const bookId = bookCard.getAttribute("data-book-id")
+      const index = myLibrary.library.findIndex((item) => bookId === item.id)
+      myLibrary.library[index].toggleRead()
+
+      myLibrary.library[index].isRead ? changeBtn.textContent = "Read"
+        : changeBtn.textContent = "Not read"
+    })
+  }
+
+  static #addDeleteBtn(bookCard) {
     const deleteBtn = document.createElement('button')
 
     deleteBtn.textContent = "Delete"
@@ -40,56 +78,35 @@ class Book {
 
     deleteBtn.addEventListener("click", () => {
       const bookId = bookCard.getAttribute("data-book-id")
-      const index = myLibrary.findIndex((item) => bookId === item.id)
+      const index = myLibrary.library.findIndex((item) => bookId === item.id)
       if (index >= 0) {
-        myLibrary.splice(index, 1)
+        myLibrary.library.splice(index, 1)
       }
       bookCard.remove();
     })
 
   }
 
-  addToggleBtn(bookCard) {
-    const changeBtn = document.createElement('button')
-
-    this.isRead ? changeBtn.textContent = "Read New"
-      : changeBtn.textContent = "Not read New"
-
-    changeBtn.classList.add("change")
-    bookCard.appendChild(changeBtn);
-
-    changeBtn.addEventListener("click", () => {
-      const bookId = bookCard.getAttribute("data-book-id")
-      const index = myLibrary.findIndex((item) => bookId === item.id)
-      this.toggleRead()
-      this.isRead ? changeBtn.textContent = "Read"
-        : changeBtn.textContent = "Not read"
-    })
-  }
-
-  toggleRead() {
-    this.isRead = !this.isRead
-  }
 }
 
+const bookList = document.querySelector("ul");
 
-const ul = document.querySelector("ul");
-const bookList = ul;
+let display = new Display()
+let myLibrary = new Library();
 
-
-myLibrary[0] = new Book("Pride and Prejudice", "Jane Austen", 338, true)
-myLibrary[1] = new Book("Frankenstein", "Mary Shelley", 194, true)
-myLibrary[2] = new Book("Harry Potter", "Joanne K. Rowling", 287, false)
-myLibrary[3] = new Book("The Hobbit", "J.R.R. Tolkien", 295, false)
-
-
-function showLibrary(myLibrary) {
-  myLibrary.forEach((item) => {
-    item.addBookToPage()
+(function() {
+  const initBooks = [
+    new Book("Pride and Prejudice", "Jane Austen", 338, true),
+    new Book("Frankenstein", "Mary Shelley", 194, true),
+    new Book("Harry Potter", "Joanne K. Rowling", 287, false),
+    new Book("The Hobbit", "J.R.R. Tolkien", 295, false)
+  ]
+  
+  initBooks.forEach((item) => {
+    myLibrary.addBook(item)
+    Display.addBook(item)
   })
-}
-
-showLibrary(myLibrary);
+})()
 
 // form to add books
 const addBtn = document.querySelector("#id_add")
@@ -104,6 +121,6 @@ function addBookForm(event) {
 
 
   const newBook = new Book(title, author, pages, isRead)
-  newBook.addBookToLibrary(myLibrary)
-  newBook.addBookToPage()
+  myLibrary.addBook(newBook) 
+  Display.addBook(newBook)
 }
